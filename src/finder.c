fbed80a9a551e6511e12d8b8d3d44817eac106a9
justinjradi <71425588+justinjradi@users.noreply.github.com>
2025-06-20 15:41:07 -0700

    heck yea

--- src/finder.c
+++ src/finder.c
@@ -37,6 +37,44 @@ const float E12_values[] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.
 const struct ComponentSeries E12 = {(float*)E12_values, 0.10,
     sizeof(E12_values) / sizeof(float)};
 
+const float E24_values[] = {1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3,
+    3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1};
+const struct ComponentSeries E24 = {(float*)E24_values, 0.05,
+    sizeof(E24_values) / sizeof(float)};
+
+const float E48_values[] = {1.00, 1.05, 1.10, 1.15, 1.21, 1.27, 1.33, 1.40, 1.47,
+    1.54, 1.62, 1.69, 1.78, 1.87, 1.96, 2.05, 2.15, 2.26, 2.37, 2.49, 2.61, 2.74, 2.87,
+    3.01, 3.16, 3.32, 3.48, 3.65, 3.83, 4.02, 4.22, 4.42, 4.64, 4.87, 5.11, 5.36, 5.62, 5.90,
+    6.19, 6.49, 6.81, 7.15, 7.50, 7.87, 8.25, 8.66, 9.09, 9.53};
+const struct ComponentSeries E48 = {(float*)E48_values, 0.02,
+    sizeof(E48_values) / sizeof(float)};
+
+const float E96_values[] = {1.00, 1.02, 1.05, 1.07, 1.10, 1.13, 1.15, 1.18,
+    1.21, 1.24, 1.27, 1.30, 1.33, 1.37, 1.40, 1.43, 1.47, 1.50, 1.54, 1.58, 1.62, 1.65, 1.69,
+    1.74, 1.78, 1.82, 1.87, 1.91, 1.96, 2.00, 2.05, 2.10, 2.15, 2.21, 2.26, 2.32, 2.37, 2.43,
+    2.49, 2.55, 2.61, 2.67, 2.74, 2.80, 2.87, 2.94, 3.01, 3.09, 3.16, 3.24, 3.32, 3.40, 3.48,
+    3.57, 3.65, 3.74, 3.83, 3.92, 4.02, 4.12, 4.22, 4.32, 4.42, 4.53, 4.64, 4.75, 4.87, 4.99,
+    5.11, 5.23, 5.36, 5.49, 5.62, 5.76, 5.90, 6.04, 6.19, 6.34, 6.49, 6.65, 6.81, 6.98, 7.15,
+    7.32, 7.50, 7.68, 7.87, 8.06, 8.25, 8.45, 8.66, 8.87, 9.09, 9.31, 9.53, 9.76};
+const struct ComponentSeries E96 = {(float*)E96_values, 0.01,
+    sizeof(E96_values) / sizeof(float)};
+
+const float E192_values[] = {1.00, 1.01, 1.02, 1.04, 1.05, 1.06, 1.07, 1.09, 1.10, 1.11, 1.13,
+    1.14, 1.15, 1.17, 1.18, 1.20, 1.21, 1.23, 1.24, 1.26, 1.27, 1.29, 1.30, 1.32, 1.33, 1.35,
+    1.37, 1.38, 1.40, 1.42, 1.43, 1.45, 1.47, 1.49, 1.50, 1.52, 1.54, 1.56, 1.58, 1.60, 1.62,
+    1.64, 1.65, 1.67, 1.69, 1.72, 1.74, 1.76, 1.78, 1.80, 1.82, 1.84, 1.87, 1.89, 1.91, 1.93,
+    1.96, 1.98, 2.00, 2.03, 2.05, 2.08, 2.10, 2.13, 2.15, 2.18, 2.21, 2.23, 2.26, 2.29, 2.32,
+    2.34, 2.37, 2.40, 2.43, 2.46, 2.49, 2.52, 2.55, 2.58, 2.61, 2.64, 2.67, 2.71, 2.74, 2.77,
+    2.80, 2.84, 2.87, 2.91, 2.94, 2.98, 3.01, 3.05, 3.09, 3.12, 3.16, 3.20, 3.24, 3.28, 3.32,
+    3.36, 3.40, 3.44, 3.48, 3.52, 3.57, 3.61, 3.65, 3.70, 3.74, 3.79, 3.83, 3.88, 3.92, 3.97,
+    4.02, 4.07, 4.12, 4.17, 4.22, 4.27, 4.32, 4.37, 4.42, 4.48, 4.53, 4.59, 4.64, 4.70, 4.75,
+    4.81, 4.87, 4.93, 4.99, 5.05, 5.11, 5.17, 5.23, 5.30, 5.36, 5.42, 5.49, 5.56, 5.62, 5.69,
+    5.76, 5.83, 5.90, 5.97, 6.04, 6.12, 6.19, 6.26, 6.34, 6.42, 6.49, 6.57, 6.65, 6.73, 6.81,
+    6.90, 6.98, 7.06, 7.15, 7.23, 7.32, 7.41, 7.50, 7.59, 7.68, 7.77, 7.87, 7.96, 8.06, 8.16,
+    8.25, 8.35, 8.45, 8.56, 8.66, 8.76, 8.87, 8.98, 9.09, 9.20, 9.31, 9.42, 9.53, 9.65, 9.76, 9.88};
+const struct ComponentSeries E192 = {(float*)E192_values, 0.005,
+    sizeof(E192_values) / sizeof(float)};
+
 struct Network
 {
     float value;
@@ -65,7 +103,7 @@ struct Network options_list[NUM_OPTIONS];
 
 // Function Declarations
 
-int init_stock();
+int init_stock(const struct ComponentSeries series);
 int deinit_stock();
 int create_symbol(float value, float tolerance, char* symbol);
 void print_stock();
@@ -83,9 +121,9 @@ void print_options();
 int main()
 {
     networkType = RESISTIVE;
-    max_size = 4;
-    target_value = 111;
-    init_stock();
+    max_size = 2;
+    target_value = 3141.59265359;
+    init_stock(E12);
     init_options_list();
 
     struct Network n;
@@ -108,10 +146,10 @@ int main()
 // Function Definitions
 
 // Creates stock of components
-int init_stock()
+int init_stock(const struct ComponentSeries series)
 {
     int start_decade = 1;
-    int end_decade = 5;
+    int end_decade = 6;
     if ((start_decade < -12) || (start_decade > 12))
     {
         printf("Error: Start decade is out of bounds.\n");
@@ -127,7 +165,7 @@ int init_stock()
         printf("Error: End decade cannot exceed start decade.\n");
         return 1;
     }
-    stock_size = E3.size * (end_decade - start_decade + 1);
+    stock_size = series.size * (end_decade - start_decade + 1);
     stock = malloc(stock_size * sizeof(struct Component));
     if (stock == NULL)
     {
@@ -135,14 +173,14 @@ int init_stock()
         return 1;
     }
     struct Component temp_component;
-    temp_component.tolerance = E3.tolerance;
+    temp_component.tolerance = series.tolerance;
     for (int d = start_decade; d <= end_decade; d++)
     {
-       for (int c = 0; c < E3.size; c++)
+       for (int c = 0; c < series.size; c++)
        {
-            temp_component.value = E3.values[c] * (int)pow(10, d);
-            create_symbol(temp_component.value, E3.tolerance, temp_component.symbol);
-            stock[(d - start_decade)*E3.size + c] = temp_component;
+            temp_component.value = series.values[c] * (int)pow(10, d);
+            create_symbol(temp_component.value, series.tolerance, temp_component.symbol);
+            stock[(d - start_decade)*series.size + c] = temp_component;
        }
     }
 }
